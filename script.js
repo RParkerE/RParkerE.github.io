04b23bba1ed59106d58f1168e405f3c3477f9871
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2024-04-16 14:03:31 -0500

    Terminal v1.2

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function getDirectoryPath() {
 }
 
 function fetchAndDisplayFile(file) {
-    fetch(`https://raw.githubusercontent.com/RParkerE/RParkerE.github.io/${file}`)
+    fetch(`https://github.com/RParkerE/RParkerE.github.io/blob/main/${file}`)
         .then(response => response.text())
         .then(data => {
             outputDiv.innerHTML += `
